cf5d0962e4d23396dede99db07b080635d66f127
Natsuo Yamamoto <n-yamamoto@nifty.com>
2019-01-25 20:07:05 +0800

    2019-01-24

--- script/y7filtera.sh
+++ script/y7filtera.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat ../json/y7words.json | jq -r '.[][1]' | awk -f y7filtera.awk > y7filtera.bak
+cat ../y7zh.json | jq -r '.[][1]' | awk -f y7filtera.awk > y7filtera.bak
 say -v Ting-Ting -r 100 -f y7filtera.bak -o ../mp3/y7zh.m4a
 
