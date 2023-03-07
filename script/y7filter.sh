09eff50ee7dd469aa4784c0ad23967b357577035
Natsuo Yamamoto <720yamamoto@gmail.com>
2023-03-07 10:52:13 +0900

    '_'

--- script/y7filter.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cat ../json/y7words.json | jq -r '.[][1]' | awk -f y7filter1.awk
-
--- script/y7filtera.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cat ../y7zh.json | jq -r '.[][1]' | awk -f y7filtera.awk > y7filtera.bak
-say -v Ting-Ting -r 100 -f y7filtera.bak -o ../mp3/y7zh.m4a
-
